1fd8fc3858c5047eb7d69c34d746d0ffadb2e3db
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-05-30 14:35:11 +0200

    Nieuwe folder

---
+++ Dido/DiDo/DiDo/GameElements/GameStateManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiDo.GameElements
+{
+    class GameStateManager
+    {
+        public static void GSManager()
+        {
+            if (MainPage.RoundEnded == true)
+            {
+                MainPage.BG = MainPage.Level2;
+            }
+            else
+            {
+
+                if (MainPage.GameState == 0)
+                {
+
+                    MainPage.BG = MainPage.StartScreen;
+
+
+
+                }
+                else if (MainPage.GameState == 1)
+                {
+                    MainPage.BG = MainPage.Level1;
+                }
+
+            }
+        }
+    }
+}
