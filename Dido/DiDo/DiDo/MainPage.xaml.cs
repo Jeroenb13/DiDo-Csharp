8f9fc2904fff81e6efa203f2017f81a28e7339e2
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-05-30 15:04:07 +0200

    Nieuwe class gemaakt, levels

--- Dido/DiDo/DiDo/MainPage.xaml.cs
+++ Dido/DiDo/DiDo/MainPage.xaml.cs
@@ -45,78 +45,6 @@ namespace DiDo
 
         public static DispatcherTimer RoundTimer = new DispatcherTimer();
 
-
-        private Dictionary<String, String> tiles = new Dictionary<String, String>()
-        {
-            { "a", "bridge" },
-            { "b", "crate" },
-            { "c", "disco" },
-            { "d", "door" },
-            { "e", "fence" },
-            { "f", "floor" },
-            { "g", "floor_cracked_1" },
-            { "h", "floor_cracked_2" },
-            { "i", "floor_cracked_3" },
-            { "j", "floor_cracked_4" },
-            { "k", "floor_cracked_5" },
-            { "l", "floor_cracked_6" },
-            { "m", "gas" },
-            { "n", "grass" },
-            { "o", "grass_flower_1" },
-            { "p", "grass_flower_2" },
-            { "q", "grass_flower_3" },
-            { "r", "grass_flower_4" },
-            { "s", "grass_flower_5" },
-            { "t", "path_0_straight" },
-            { "u", "path_0_turn" },
-            { "v", "path_0_cross" },
-            { "w", "path_0_t" },
-            { "x", "path_90_straight" },
-            { "y", "path_90_turn" },
-            { "z", "path_90_cross" },
-            { "A", "path_90_t" },
-            { "B", "path_180_straight" },
-            { "C", "path_180_turn" },
-            { "D", "path_180_cross" },
-            { "E", "path_180_t" },
-            { "F", "path_270_straight" },
-            { "G", "path_270_turn" },
-            { "H", "path_270_cross" },
-            { "I", "path_270_t" },
-            { "J", "rubbish_bin" },
-            { "K", "seat" },
-            { "L", "table" },
-            { "M", "torch_0" },
-            { "N", "torch_90" },
-            { "O", "torch_180" },
-            { "P", "torch_270" },
-            { "Q", "tree" },
-            { "R", "wall_0_straight" },
-            { "S", "wall_0_turn" },
-            { "T", "wall_90_straight" },
-            { "U", "wall_90_turn" },
-            { "V", "wall_180_straight" },
-            { "W", "wall_180_turn" },
-            { "X", "wall_270_straight" },
-            { "Y", "wall_270_turn" },
-            { "Z", "water" },
-            { "!", "new_row" }
-        };
-
-        String levelOne = "SRRMRRRRRMRRRU"
-                    + "!" + "XffffffffffffT"
-                    + "!" + "XffgfffffffffT"
-                    + "!" + "XfffffffffffbT"
-                    + "!" + "XffffbfhffffbN"
-                    + "!" + "XffffbbffffffT"
-                    + "!" + "XfifffffffffbT"
-                    + "!" + "XffffffffffffT"
-                    + "!" + "XffffffjfffffN"
-                    + "!" + "XbbfffffffkffT"
-                    + "!" + "XfbffffffffffT"
-                    + "!" + "XfblffffbffffT"
-                    + "!" + "YVVVVVVVVVVVVW";
-
         public MainPage()
         {
             this.InitializeComponent();
@@ -127,8 +55,6 @@ namespace DiDo
             RoundTimer.Tick += RoundTimer_Tick;
             RoundTimer.Interval = new TimeSpan(0, 0, 1);
             Window.Current.CoreWindow.KeyDown += CoreWindow_Keydown;
-
-
 
 
 
@@ -240,7 +166,7 @@ namespace DiDo
             {
                 GameState = 0;
                 RoundEnded = false;
-                countdown = 6;
+                countdown = 60;
 
             }
             else
