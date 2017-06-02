4c12612da569a92e751ef12703035e512a6aa0ae
Daan <daan@haitsma-dejager.nl>
2017-06-02 10:28:48 +0200

    bullets werken nu want daan kan wel dingen

---
+++ Dido/DiDo/DiDo/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiDo
+{
+    public class Player : Entity
+    {
+        public Player(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
