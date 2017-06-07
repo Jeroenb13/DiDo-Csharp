d718791a15a1c8b3416237616e0b0ced5de88d34
Jeroenb13 <jeroen.bakker@student.stenden.com>
2017-06-07 14:33:53 +0200

    Menupagina Setting pagina en helppagina gemaakt

--- Dido/DiDo/DiDo/App.xaml.cs
+++ Dido/DiDo/DiDo/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DiDo.MenuFolder;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -72,7 +73,7 @@ namespace DiDo
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Menu), e.Arguments);
+                    rootFrame.Navigate(typeof(MenuPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
