10285bf42b1e56333d0d90fd77c17e8e9a92e011
HayriHeling <hayriheling@hotmail.com>
2017-06-12 11:41:46 +0200

    Placed movement in ClientPlayer

--- Dido/DiDo/DiDo/Character/ClientPlayer.cs
+++ Dido/DiDo/DiDo/Character/ClientPlayer.cs
@@ -3,13 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
 
 namespace DiDo.Character
 {
     public abstract class ClientPlayer : Player
     {
+        public Dictionary<VirtualKey, Boolean> keysPressed = new Dictionary<VirtualKey, bool>();
+        Window.Current.CoreWindow.KeyDown += CoreWindow_Keydown;
+        Window.Current.CoreWindow.KeyUp += CoreWindow_Keyup;
+
         public ClientPlayer(string name, float x, float y) : base(name, x, y)
         {
+
+        }
+
+        //Character Movement 
+        private void CoreWindow_Keydown(CoreWindow sender, KeyEventArgs args)
+        {
+            //int move_speed = 5;
+
+            keysPressed[args.VirtualKey] = true;
+
+            //to do keylijst maken keylijst
+            if (args.VirtualKey == VirtualKey.A)
+            {
+                velX = move_speed;
+            }
+            else if (args.VirtualKey == VirtualKey.D)
+            {
+                velX = move_speed;
+            }
+            else if (args.VirtualKey == VirtualKey.W)
+            {
+                velY = move_speed;
+            }
+            else if (args.VirtualKey == VirtualKey.S)
+            {
+                velY = move_speed;
+            }
+        }
+
+        private void CoreWindow_Keyup(CoreWindow sender, KeyEventArgs args)
+        {
+
+            keysPressed[args.VirtualKey] = false;
+
+            //to do keylijst maken keylijst
+            if (args.VirtualKey == VirtualKey.A)
+            {
+                velX = 0;
+            }
+            else if (args.VirtualKey == VirtualKey.D)
+            {
+                velX = 0;
+            }
+            else if (args.VirtualKey == VirtualKey.W)
+            {
+                velY = 0;
+            }
+            else if (args.VirtualKey == VirtualKey.S)
+            {
+                velY = 0;
+            }
+
         }
     }
 }
