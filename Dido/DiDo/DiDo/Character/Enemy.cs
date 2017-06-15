d143c54118c9b03004d5eb0ac0a957c4faabef6d
Jordy Kroeze <jordy.kroeze@student.stenden.com>
2017-06-15 10:58:45 +0200

    Enemies toegevoegd (Lopen nog niet)

---
+++ Dido/DiDo/DiDo/Character/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiDo.Character
+{
+    public class Enemy
+    {
+        public String name { get; }
+        public int Health { get; set; }
+        public float x { get; set; }
+        public float y { get; set; }
+
+        public Enemy(String name, float x, float y)
+        {
+            this.name = name;
+            this.Health = 5;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void hit(int damage = 1)
+        {
+            this.Health = this.Health - damage;
+        }
+
+        public int health()
+        {
+            return this.Health;
+        }
+
+        public String debugName()
+        {
+            return this.name + " (" + this.health() + ")";
+        }
+
+    }
+}
