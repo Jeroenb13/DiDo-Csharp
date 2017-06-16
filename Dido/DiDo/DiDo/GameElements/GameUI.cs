3ba7e96254d106b64422d7a5ff580e0e5c1a53ff
HayriHeling <hayriheling@hotmail.com>
2017-06-16 12:11:06 +0200

    Added characters, ui elements to assets and updated the ui element itself

---
+++ Dido/DiDo/DiDo/GameElements/GameUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiDo.Character;
+
+namespace DiDo.GameElements
+{
+    class GameUI
+    {
+        public GameUI()
+        {
+            
+        }
+
+        public void SetUiElements(int health)
+        {
+            
+        }
+    }
+}
