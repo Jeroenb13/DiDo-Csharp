f2e02ef1048be70071ff7133e626dc99dede9955
Daan <daan@haitsma-dejager.nl>
2017-06-16 10:32:10 +0200

    Translated a bunch of comments from Dutch to English

--- Dido/DiDo/DiDo/MainPage.xaml.cs
+++ Dido/DiDo/DiDo/MainPage.xaml.cs
@@ -56,7 +56,7 @@ namespace DiDo
 
         public List<Enemy> enemies = new List<Enemy>();
 
-        public float temp_x, temp_y; // Tijdelijk
+        public float temp_x, temp_y; // Temporary
 
         public double frames = 0;
 
@@ -115,10 +115,10 @@ namespace DiDo
                 }
             }
 
-            args.DrawingSession.DrawImage(ImageManipulation.image(Arms_sprite, radians(mousePoint, playerPoint)), player.x, player.y); // Later zorgen dat de scaling en rotation niet elke frame gebeurt
-            args.DrawingSession.DrawImage(ImageManipulation.image(Player_sprite, radians(mousePoint, playerPoint)), player.x, player.y); // Later zorgen dat de scaling en rotation niet elke frame gebeurt
+            args.DrawingSession.DrawImage(ImageManipulation.image(Arms_sprite, radians(mousePoint, playerPoint)), player.x, player.y);
+            args.DrawingSession.DrawImage(ImageManipulation.image(Player_sprite, radians(mousePoint, playerPoint)), player.x, player.y); // TODO: make it so that scaling and rotation is not processed each frame
             args.DrawingSession.DrawText("X1: " + xPos + " | Y1: " + yPos + " | X1: " + xPos2 + " | Y1: " + yPos2 + " | Type: " + levels.getTileType(player.x, player.y, levels.gekozenLevel), 10, 600, Colors.Black); // Toon welke Tile de player is, Tijdelijk
-            args.DrawingSession.DrawText("Player X: " + player.x + " | Player Y: " + player.y, 10, 650, Colors.Black); // Toon de player location, Tijdelijk
+            args.DrawingSession.DrawText("Player X: " + player.x + " | Player Y: " + player.y, 10, 650, Colors.Black); // Show the player location, temporary
             args.DrawingSession.DrawText("Inventory World: " + inventory(), 10, 700, Colors.Black);
             args.DrawingSession.DrawText("Inventory Player: " + player.inventory(), 300, 700, Colors.Black);
             //args.DrawingSession.DrawText("InHand: " + player.currentWeapon.name + " " + player.currentWeapon.getAmmo(), 10, 750, Colors.Black);
@@ -147,7 +147,7 @@ namespace DiDo
             return inventory;
         }
 
-        public double xPos, yPos, xPos2, yPos2; // tijdelijk
+        public double xPos, yPos, xPos2, yPos2; // Temporary
                                                 //public String type_tile;
 
        
@@ -215,9 +215,9 @@ namespace DiDo
             Window.Current.CoreWindow.KeyDown += controller.CoreWindow_Keydown;
             Window.Current.CoreWindow.KeyUp += controller.CoreWindow_Keyup;
 
-            this.enemies.Add(new Enemy("Freek", 128, 32)); // De AI Enemy 1
-            this.enemies.Add(new Enemy("Albert", 192, 96)); // De AI Enemy 2
-            this.enemies.Add(new Enemy("Karel", 256, 128)); // De AI Enemy 3
+            this.enemies.Add(new Enemy("Freek", 128, 32)); // The AI Enemy 1
+            this.enemies.Add(new Enemy("Albert", 192, 96)); // The AI Enemy 2
+            this.enemies.Add(new Enemy("Karel", 256, 128)); // The AI Enemy 3
         }
         
         public void updatePoint(Player player)
@@ -300,7 +300,7 @@ namespace DiDo
             Pistol = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Items/gun-3.png"));
             Bullets = ImageManipulation.img(Bullet);
             Enemy1 = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Char/spr_hayri.png"));
-             // Zodat dit niet elk frame gebeurt maar slechts eenmalig
+             // So that this isn't done on each frame, but only once.
 
             foreach (Tile t in Levels.Levels.tiles.Values)
             {
@@ -333,9 +333,9 @@ namespace DiDo
                         enemy.hit(bullet.damage);
                         if(enemy.health() <= 0)
                         {
-                            enemiesToRemove.Add(emeniesCount); // Enemy klaar zetten om te verwijderen
+                            enemiesToRemove.Add(emeniesCount); // Prepare enemy to be removed
                         }
-                        bulletsToRemove.Add(bullet); // kogel na een hit verwijderen
+                        bulletsToRemove.Add(bullet); // Remove the bullet upon impact
                     }
                     emeniesCount++;
                 }
@@ -381,7 +381,7 @@ namespace DiDo
                     float xVel = xPos - player.x;
                     float yVel = yPos - player.y;
 
-                    // pythagorasmagie
+                    // pythagorasmagic
                     float distance = (float)Math.Sqrt(Math.Pow((double)xVel, 2) + Math.Pow((double)yVel, 2));
                     float scaling = distance / 25;
 
