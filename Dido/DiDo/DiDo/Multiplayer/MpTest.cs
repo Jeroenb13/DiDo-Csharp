5097a22053abe20cfcd5c606b6c4badea4d6ce8f
jk-5 <jeffrey.kog@gmail.com>
2017-06-16 09:32:13 +0200

    Added a simple heap buffer implementation

---
+++ Dido/DiDo/DiDo/Multiplayer/MpTest.cs
@@ -0,0 +1,17 @@
+﻿using DiDoCommon.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiDo.Multiplayer
+{
+    class MpTest
+    {
+        public void Test()
+        {
+            HeapByteBuf buf = new HeapByteBuf(10, 50);
+        }
+    }
+}
---
+++ Dido/DiDo/DiDoCommon/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DiDoCommon
+{
+    public class Class1
+    {
+    }
+}
