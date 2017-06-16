cb9973d6729d8819532865cf057f88d3fc8c6d56
HayriHeling <hayriheling@hotmail.com>
2017-06-16 10:38:02 +0200

    Basic UI element set

--- Dido/DiDo/DiDo/MainPage.xaml.cs
+++ Dido/DiDo/DiDo/MainPage.xaml.cs
@@ -31,6 +31,7 @@ namespace DiDo
         public static CanvasBitmap BG, StartScreen, Bullet, Enemy1, Enemy2, Player_sprite;
         public static Transform2DEffect Bullets, PlayerA, PlayerS, PlayerD, PlayerW;
         public static Rect bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+        public Rect ui = new Rect(15, 500, 800, 100); //UI element 
         public static float DesignWidth = 1920;
         public static float DesignHeight = 1080;
         public static float scaleWidth, scaleHeight, pointX, pointY;
@@ -91,13 +92,16 @@ namespace DiDo
 
             //Debug
             args.DrawingSession.DrawImage(ImageManipulation.image(Player_sprite, radians(mousePoint, playerPoint)), player.x, player.y); // Later zorgen dat de scaling en rotation niet elke frame gebeurt
-            args.DrawingSession.DrawText("X1: " + xPos + " | Y1: " + yPos + " | X1: " + xPos2 + " | Y1: " + yPos2 + " | Type: " + levels.getTileType(player.x, player.y, levels.gekozenLevel), 10, 600, Colors.Black); // Toon welke Tile de player is, Tijdelijk
-            args.DrawingSession.DrawText("Player X: " + player.x + " | Player Y: " + player.y, 10, 650, Colors.Black); // Toon de player location, Tijdelijk
-            args.DrawingSession.DrawText("Inventory: " + player.inventory(), 10, 700, Colors.Black);
-            args.DrawingSession.DrawText("InHand: " + player.currentWeapon.name + " " + player.currentWeapon.getAmmo(), 10, 750, Colors.Black);
-            args.DrawingSession.DrawText("Player Point: " + playerPoint, 10, 550, Colors.Black);
-            args.DrawingSession.DrawText("Mouse Point: " + mousePoint, 10, 500, Colors.Black);
-            args.DrawingSession.DrawText("Radians: " + radians(playerPoint, mousePoint), 10, 450, Colors.Black);
+            //args.DrawingSession.DrawText("X1: " + xPos + " | Y1: " + yPos + " | X1: " + xPos2 + " | Y1: " + yPos2 + " | Type: " + levels.getTileType(player.x, player.y, levels.gekozenLevel), 10, 600, Colors.Black); // Toon welke Tile de player is, Tijdelijk
+            //args.DrawingSession.DrawText("Player X: " + player.x + " | Player Y: " + player.y, 10, 650, Colors.Black); // Toon de player location, Tijdelijk
+            //args.DrawingSession.DrawText("Player Point: " + playerPoint, 10, 550, Colors.Black);
+            //args.DrawingSession.DrawText("Mouse Point: " + mousePoint, 10, 500, Colors.Black);
+            //args.DrawingSession.DrawText("Radians: " + radians(playerPoint, mousePoint), 10, 450, Colors.Black);
+            args.DrawingSession.DrawText("Inventory: " + player.inventory(), 25, 525, Colors.Black);
+            args.DrawingSession.DrawText("InHand: " + player.currentWeapon.name, 225, 525, Colors.Black);
+            args.DrawingSession.DrawText("Ammo: " + player.currentWeapon.getAmmo(), 425, 525, Colors.Black);
+            args.DrawingSession.DrawText("Health: " + player.getHealth(), 625, 525, Colors.Black);
+            args.DrawingSession.DrawRectangle(ui, Colors.Black); //UI element (5, 500, 800, 100)
             //Debug end
 
 
