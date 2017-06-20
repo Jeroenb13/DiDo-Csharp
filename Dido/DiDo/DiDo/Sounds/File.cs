c4f2c63e732007cc63c29ca7025221f5c4fad226
HayriHeling <hayriheling@hotmail.com>
2017-06-20 12:36:31 +0200

    Added dictionary for sounds

---
+++ Dido/DiDo/DiDo/Sounds/File.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiDo.Sounds
+{
+    class File
+    {
+        public string fileName { get; }
+        public Uri file { get; }
+
+        public File(string fileName, Uri file)
+        {
+            this.fileName = fileName;
+            this.file = file;
+        }
+    }
+}
