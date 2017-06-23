af7e770b3034f6abff0015e7e1148aab70156c63
ElderMatt <matthew.elderhorst@hotmail.com>
2017-06-23 14:41:32 +0200

    Added Comments

--- Dido/DiDo/DiDo/Character/Entity.cs
+++ Dido/DiDo/DiDo/Character/Entity.cs
@@ -11,16 +11,29 @@ namespace DiDo
     {
         private Position position;
 
+        /// <summary>
+        /// Constructor of the entity
+        /// </summary>
+        /// <param name="x">X coördinate of the entity</param>
+        /// <param name="y">Y coördinate of the entity</param>
         public Entity(float x, float y) : base(x, y)
         {
 
         }
 
+        /// <summary>
+        /// Returns the position
+        /// </summary>
+        /// <returns></returns>
         public Position getPosition()
         {
             return position;
         }
 
+        /// <summary>
+        /// Sets the position
+        /// </summary>
+        /// <param name="position">Sets the X and Y position</param>
         protected void setPosition(Position position)
         {
             this.position = position;
--- Dido/DiDo/DiDo/Character/Player.cs
+++ Dido/DiDo/DiDo/Character/Player.cs
@@ -12,6 +12,17 @@ namespace DiDo
     {
         public float velX { get; set; } = 0;
         public float velY { get; set; } = 0;
+
+        /// <summary>
+        /// Constructor of the MyPlayer
+        /// </summary>
+        /// <param name="name">Name the the Player</param>
+        /// <param name="maxHealth">Maximum health of the Player</param>
+        /// <param name="healthPoints">Current Health of the Player</param>
+        /// <param name="stamina">Maximum amount of stamina the Player can have</param>
+        /// <param name="move_speed">Move speed of the Player</param>
+        /// <param name="x">X coördinate of the Player</param>
+        /// <param name="y">Y coördinate of the Player</param>
         public Player(String name, int maxHealth, int healthPoints, int stamina, int move_speed, float x, float y) : base(name, maxHealth, healthPoints, stamina, move_speed, x, y)
         {
 
